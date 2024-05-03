d79806bcf52a04a10a2ae3c7f325ac3319e7f78d
Om Sevak <omsevak252002@gmail.com>
2024-05-03 16:37:47 -0500

    Boilerplate code for client and server

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
+import Homepage from './pages/Homepage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        Lets develop jarvis!!
+      </div>
+    );
+  }
+}
+
+export default HomePage;
