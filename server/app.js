a0910b1857141e436221906c985a344ec4b1db20
Om Sevak <omsevak252002@gmail.com>
2024-05-09 19:44:07 -0400

    Demo commit

--- server/app.js
+++ server/app.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
+app.use(express.json());
 
+
+app.post('/login', (req, res) => {
+   res.send('This is login validation logic');
+   });
+
+app.post('/register', (req, res) => {
+
+   console.log('Request body', req.body);
+
+   const name = req.body.name;
+
+   res.send('Hello ' + name + ' you are registered successfully');
+   });
+
+
+   
 module.exports = app;
