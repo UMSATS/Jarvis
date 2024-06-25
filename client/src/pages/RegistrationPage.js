338e81fba13414cbd2810096d1c9d03ddfca3e5c
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-06-24 23:12:56 -0500

    update: working on handler

--- client/src/pages/RegistrationPage.js
+++ client/src/pages/RegistrationPage.js
@@ -16,66 +16,81 @@ import FormControl from '@mui/material/FormControl';
 
 class RegistrationPage extends React.Component {    
     render() {
+        const registerHandler = () => {
+            console.log('Registering user...');
+        }
         return (
             <div>
                 <Box
                     component="form"
                     sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                        justifyContent: 'center',
+                        alignItems: 'center'
                     }}
                     noValidate
                     autoComplete="off"
                 >
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <div>
-                            <FormControl>
-                                <TextField
-                                    required
-                                    id="outlined-required"
-                                    label="First Name"
-                                    variant="outlined"
-                                />
-                            </FormControl>
-                        </div>
-                        <div>
+                    <div style={{ display: 'flex'}}>
+                        <FormControl>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="First Name"
+                                variant="outlined"
+                            />
+                        </FormControl>
+                        <FormControl>
                             <TextField
                                 required
                                 id="outlined-required"
                                 label="Last Name"
-                                defaultValue="Last Name"
                                 variant="outlined"
                             />
-                        </div>
+                        </FormControl>
                     </div>
-                    <div>
-                        // Text field for email
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Email"
-                            defaultValue="Email"
-                            variant="outlined"
-                        />
+                    <div style={{ display: 'flex'}}>
+                        <FormControl>
+                            <FormHelperText>Enter a valid email address with domain @umsats.ca</FormHelperText>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Email"
+                                variant="outlined"
+                            />
+                        </FormControl>
                     </div>
-                    <div>
-                        // Text field for password
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Password"
-                            defaultValue="Password"
-                            variant="outlined"
-                        />
+                    <div style={{ display: 'flex'}}>
+                        <FormControl>
+                            <FormHelperText>Enter a valid UMSATS username</FormHelperText>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Username"
+                                variant="outlined"
+                            />
+                        </FormControl>
                     </div>
-                    <div>
-                        // Text field for confirm password
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Confirm Password"
-                            defaultValue="Confirm Password"
-                            variant="outlined"
-                        />
+                    <div style={{ display: 'flex'}}>
+                        <FormControl>
+                            <FormHelperText>Enter a password with at least 8 characters</FormHelperText>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Password"
+                                variant="outlined"
+                            />
+                        </FormControl>
+                    </div>
+                    <div style={{ display: 'flex'}}>
+                        <FormControl>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Confirm Password"
+                                variant="outlined"
+                            />
+                        </FormControl>
                     </div>
                 </Box>
                 
