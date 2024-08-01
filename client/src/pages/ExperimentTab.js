35582aec24a47c35c2f53cfc8817fc93819d9079
ArnavG-it <arnavgupta468@outlook.com>
2024-07-31 21:59:04 -0500

    Rename TemperatureChart.jsx to ExperimentChart.jsx

--- client/src/pages/ExperimentTab.js
+++ client/src/pages/ExperimentTab.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TemperatureChart from '../components/TemperatureChart.jsx';
+import TemperatureChart from '../components/ExperimentChart.jsx';
 import Box from '@mui/material/Box';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControl from '@mui/material/FormControl';
