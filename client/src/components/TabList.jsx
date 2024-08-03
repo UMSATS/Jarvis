30837d938609184672f929818270b394d2a7b92d
ArnavG-it <arnavgupta468@outlook.com>
2024-08-03 09:58:37 -0500

    Add gradient icons when selected

--- client/src/components/TabList.jsx
+++ client/src/components/TabList.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import List from '@mui/material/List';
 import TabListItem from './TabListItem.jsx';
+
 import { ReactComponent as DashboardIcon} from '../assets/images/icons/grey/dashboard.svg';
 import { ReactComponent as ExperimentIcon } from '../assets/images/icons/grey/experiment.svg';
 import { ReactComponent as EventLogIcon } from '../assets/images/icons/grey/event_log.svg';
 import { ReactComponent as OrientationIcon } from '../assets/images/icons/grey/orientation.svg';
 import { ReactComponent as BatteryIcon } from '../assets/images/icons/grey/battery.svg';
 import { ReactComponent as TemperaturesIcon } from '../assets/images/icons/grey/temperatures.svg';
+
+import { ReactComponent as DashboardIconGradient} from '../assets/images/icons/gradient/dashboard.svg';
+import { ReactComponent as ExperimentIconGradient } from '../assets/images/icons/gradient/experiment.svg';
+import { ReactComponent as EventLogIconGradient } from '../assets/images/icons/gradient/event_log.svg';
+import { ReactComponent as OrientationIconGradient } from '../assets/images/icons/gradient/orientation.svg';
+import { ReactComponent as BatteryIconGradient } from '../assets/images/icons/gradient/battery.svg';
+import { ReactComponent as TemperaturesIconGradient } from '../assets/images/icons/gradient/temperatures.svg';
 
 const iconProps = {
     width: '42px',
@@ -16,12 +24,30 @@ const iconProps = {
 export default function TabList() {
     return (
         <List disablePadding>
-          <TabListItem icon={<DashboardIcon {...iconProps} />} name='Dashboard' href='/dashboard'/>
-		  <TabListItem icon={<EventLogIcon {...iconProps} />} name='Event Log' href='/event-log'/>
-		  <TabListItem icon={<TemperaturesIcon {...iconProps} />} name='Temperatures' href='/temperatures'/>
-		  <TabListItem icon={<BatteryIcon {...iconProps} />} name='Battery' href='/battery'/>
-		  <TabListItem icon={<ExperimentIcon {...iconProps} />} name='Experiment' href='/experiment'/>
-		  <TabListItem icon={<OrientationIcon {...iconProps} />} name='Orientation' href='/orientation'/>
+            <TabListItem name='Dashboard' href='/dashboard'
+                         iconUnselected={<DashboardIcon {...iconProps} />} 
+                         iconSelected={<DashboardIconGradient {...iconProps} />}
+            />
+		    <TabListItem name='Event Log' href='/event-log'
+                         iconUnselected={<EventLogIcon {...iconProps} />}
+                         iconSelected={<EventLogIconGradient {...iconProps} />}
+            />
+            <TabListItem name='Temperatures' href='/temperatures'
+                         iconUnselected={<TemperaturesIcon {...iconProps} />}
+                         iconSelected={<TemperaturesIconGradient {...iconProps} />}
+            />
+            <TabListItem name='Battery' href='/battery'
+                         iconUnselected={<BatteryIcon {...iconProps} />}
+                         iconSelected={<BatteryIconGradient {...iconProps} />}
+            />
+            <TabListItem name='Experiment' href='/experiment'
+                         iconUnselected={<ExperimentIcon {...iconProps} />}
+                         iconSelected={<ExperimentIconGradient {...iconProps} />}
+            />
+            <TabListItem name='Orientation' href='/orientation'
+                         iconUnselected={<OrientationIcon {...iconProps} />}
+                         iconSelected={<OrientationIconGradient {...iconProps} />}
+            />
         </List>
     );
 }
--- client/src/components/TabListItem.jsx
+++ client/src/components/TabListItem.jsx
@@ -4,7 +4,7 @@ import * as styles from '../styled-components/TabListItemStyles.js';
 import { useLayoutContext } from './PageLayout.jsx';
 import { useNavigate } from 'react-router-dom';
 
-export default function TabListItem({ href, icon, name }) {
+export default function TabListItem({ href, iconUnselected, iconSelected, name }) {
     const selected = window.location.pathname === href;
     const navigate = useNavigate();
     const Context = useLayoutContext();
@@ -28,7 +28,7 @@ export default function TabListItem({ href, icon, name }) {
                                              onClick={handleClick}
                                              disableRipple
                 >
-                    {icon}
+                    {selected ? iconSelected : iconUnselected}
                     <styles.StyledListItemText 
                         primary={name}
                         primaryTypographyProps={{
