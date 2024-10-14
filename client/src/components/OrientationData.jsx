1857bf9779dac8e3c9f750b21d5927544a7efe49
ArnavG-it <arnavgupta468@outlook.com>
2024-10-14 16:54:11 -0500

    Set up quaternion rotations

---
+++ client/src/components/OrientationData.jsx
@@ -0,0 +1,19 @@
+/* Quaternions are in the form [w, x, y, z] */
+
+const BASIS = [0, 0, 0, 1]
+
+export const timestamps = [
+  0, // Corresponds to the basis orientation
+  1,
+  2,
+  3,
+  4
+]
+
+export const rotations = [
+  BASIS,
+  [0, 0.7071, 0, 0.7071],   // 90 degrees about y axis
+  [-0.7071, 0, 0, 0.7071],  // -90 degrees about x axis
+  [0, 0, 0.7071, 0.7071],
+  [0, -0.7071, 0, 0.7071]
+]
--- client/src/components/OrientationView.jsx
+++ client/src/components/OrientationView.jsx
@@ -1,5 +1,7 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
+import { timestamps, rotations } from './OrientationData.jsx';
 
 export default function View() {
     return (
@@ -14,14 +16,41 @@ export default function View() {
     )
 }
 
+// Orients the basis axes of the mesh for better visualization
+const ORIENTATION_OFFSET = [Math.PI / 8, -Math.PI / 4, 0];
+
 function SatelliteMesh() {
     const meshRef = useRef();
+    const [timestamp, setTimestamp] = useState(0);
+
+    const basisEuler = new THREE.Euler(...ORIENTATION_OFFSET)
+    const basisQuaternion = new THREE.Quaternion().setFromEuler(basisEuler);
+
+    // Applies rotations for each previous timestamp.
+    /*
+    This should be moved into a useEffect() when timestamp becomes dynamic.
+    It could also be very slow - it applies every rotation beginning from t=0,
+    and could be made better depending on what the real data will look like.
+    */ 
+    const newOrientation = basisQuaternion.clone()
+    for (let i = 0; i <= timestamp; i++) {
+        const rotation = new THREE.Quaternion(
+            rotations[i][0],
+            rotations[i][1],
+            rotations[i][2],
+            rotations[i][3]
+        )
+        newOrientation.multiply(rotation).normalize();
+    }
+
+    const orientation = new THREE.Euler().setFromQuaternion(newOrientation);
+    
     return (
         <>
             <mesh
                 ref={meshRef}
                 scale={2}
-                rotation={[Math.PI / 8, -Math.PI / 4, 0]}
+                rotation={orientation}
             >
                 <boxGeometry args={[1, 1, 1]} />
                 <meshStandardMaterial color='orange' />
