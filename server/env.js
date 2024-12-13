4af462a432c036683f44412356ea2d1912e6fb29
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-12-13 12:52:42 -0600

    created server setting

---
+++ server/env.js
@@ -0,0 +1,6 @@
+const db_url = process.env.DB_HOST;
+const db_username = process.env.DB_USER;
+const db_password = process.env.DB_PASS;
+const api_port = 5000;
+
+export { db_url, db_username, db_password, api_port };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const { InfluxDB } = require('@influxdata/influxdb-client')
 const app = require('./app')
 const port = 5000
+const { db_url, db_username, db_password } = require('./env')
+const influxDB = new InfluxDB({ url: db_url, token: `${db_username}:${db_password}` })
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
