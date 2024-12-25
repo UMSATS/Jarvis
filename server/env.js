9160ce61235c6a4480eab2af697ee1703cc9094c
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-12-25 16:51:39 -0600

    containerized express

--- server/env.js
+++ server/env.js
@@ -1,6 +1,7 @@
-const db_url = process.env.DB_HOST;
+const db_url = "http://" + process.env.DB_HOST + ":" + process.env.DB_PORT;
 const db_username = process.env.DB_USER;
 const db_password = process.env.DB_PASS;
+const db_adminToken = process.env.DB_TOKEN;
 const api_port = 5000;
 
-export { db_url, db_username, db_password, api_port };
+module.exports = { db_url, db_username, db_password, db_adminToken, api_port };
--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const { InfluxDB } = require('@influxdata/influxdb-client')
 const app = require('./app')
 const port = 5000
-const { db_url, db_username, db_password } = require('./env')
-const influxDB = new InfluxDB({ url: db_url, token: `${db_username}:${db_password}` })
+const { db_url, db_username, db_password, db_adminToken } = require('./env')
+const influxDB = new InfluxDB({ url: db_url, token: db_adminToken })
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
