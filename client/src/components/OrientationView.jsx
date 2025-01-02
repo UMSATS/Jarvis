83ea90746e6acb24d8be885df48268779c2f2564
ArnavG-it <arnavgupta468@outlook.com>
2025-01-02 11:00:03 -0600

    Replace cube with TSAT 7 model

--- client/src/components/OrientationView.jsx
+++ client/src/components/OrientationView.jsx
@@ -1,35 +1,33 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
+import { useLoader } from '@react-three/fiber';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
 
 export default function View({ timestamp, rotationData }) {
     return (
         <Canvas>
-            <SatelliteMesh timestamp={timestamp} rotations={rotationData}/>
-
-            { /* Using lighting example from R3F introduction */ }
+            <SatelliteModel timestamp={timestamp} rotations={rotationData}/>
+            {/* Lighting needs an improvement */}
             <ambientLight intensity={Math.PI / 2} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         </Canvas>
     )
 }
 
-// Orients the basis axes of the mesh for better visualization
-const ORIENTATION_OFFSET = [Math.PI / 8, -Math.PI / 4, 0];
+function SatelliteModel({ timestamp, rotations }) {
+    const model = useLoader(STLLoader, "/TSAT-7_MODEL.STL");
 
-function SatelliteMesh({ timestamp, rotations }) {
-    const meshRef = useRef();
+    // Orients the basis axes of the mesh for better visualization
+    const ORIENTATION_OFFSET = [-Math.PI / 3, 0, Math.PI / 4];
 
     const basisEuler = new THREE.Euler(...ORIENTATION_OFFSET)
     const basisQuaternion = new THREE.Quaternion().setFromEuler(basisEuler);
 
-    // Applies rotations for each previous timestamp.
-    /*
-    This should be moved into a useEffect() when timestamp becomes dynamic.
-    It could also be very slow - it applies every rotation beginning from t=0,
-    and could be made better depending on what the real data will look like.
-    */ 
+    /* 
+    This performs every rotation whenever the timestamp changes -
+    it might get slow when there is a realistic number of timestamps.
+    */
     const newOrientation = basisQuaternion.clone()
     for (let i = 0; i <= timestamp; i++) {
         const rotation = new THREE.Quaternion(
@@ -42,26 +40,26 @@ function SatelliteMesh({ timestamp, rotations }) {
     }
 
     const orientation = new THREE.Euler().setFromQuaternion(newOrientation);
-    
+
     return (
         <>
-            <mesh
-                ref={meshRef}
-                scale={2}
+            <mesh 
                 rotation={orientation}
+                // This scale should be applied to the STL directly
+                scale={[0.7, 0.7, 0.7]}
             >
-                <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color='orange' />
-
-                <Arrow axis='x'/>
-                <Arrow axis='y'/>
-                <Arrow axis='z'/>
+                <primitive object={model}/>
+                <meshStandardMaterial
+                    color="orange"
+                    roughness={0.8}
+                    metalness={0.4}
+                />
             </mesh>
-            
         </>
-    )
+    );
 }
 
+// Unused now that we have the model - might be useful later on
 function Arrow({ axis }) {
     const RADIUS = 0.04;
     const LENGTH = 0.4;
