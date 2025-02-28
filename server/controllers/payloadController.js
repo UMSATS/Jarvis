16839136d308c2cabc89ee5db60ea264a184d80d
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2025-02-28 00:03:47 -0600

    server: refactor payloadController

--- server/controllers/payloadController.js
+++ server/controllers/payloadController.js
@@ -8,113 +8,71 @@ const { logInfoMsgPrefix, logWarnMsgPrefix, logErrorMsgPrefix } = require('../ut
  */
 
 /**
- * @brief This function is used to get the temperture data of a well, the data is fetched from the database
+ * @brief This function is used to get the data of a well, the data is fetched from the database
  * @param wellNum The well number
  * @query period The period of time to get the data
+ * @param field The field to fetch (temperature or luminosity)
  */
-exports.wellsTemperature = async (req, res) => {
-    console.log(logInfoMsgPrefix('Payload wells temperature check'), 'request_body:', req.body);
+const getWellData = async (req, res, field) => {
+    console.log(logInfoMsgPrefix(`Payload wells ${field} check`), 'request_body:', req.body);
     const wellNum = req.params.wellNum;
     var { period } = req.query; // extract query parameters
 
     // check if well number is provided, shouldn't happen
-    if(!wellNum){
+    if (!wellNum) {
         console.log(logWarnMsgPrefix('Well number not provided'));
-        res.status(400).json({error: 'Well number not provided'});
+        res.status(400).json({ error: 'Well number not provided' });
         return;
     }
 
     // check if well number is valid
-    if(wellNum < 1 || wellNum > 16){
+    if (wellNum < 1 || wellNum > 16) {
         console.log(logWarnMsgPrefix(`Invalid well number provided: ${wellNum}`));
-        res.status(400).json({error: 'Invalid well number provided, must be between 1 and 16'});
+        res.status(400).json({ error: 'Invalid well number provided, must be between 1 and 16' });
         return;
     }
 
     // check if period is provided, if not, set period to 1h
-    if(!period){
+    if (!period) {
         console.log(logWarnMsgPrefix('Period not provided, setting to 1h'));
         period = '1h';
-    }else{
+    } else {
         // check if period is valid, the period should be in the format of 1h, 1d, 1w, 1m, 1y, the digit can be any number except negative
         const periodRegex = /^[0-9]\d*[hwdmy]$/;
-        if(!periodRegex.test(period)){
+        if (!periodRegex.test(period)) {
             console.log(logWarnMsgPrefix(`Invalid period provided: ${period}`));
-            res.status(400).json({error: 'Invalid period provided, must be in the format of 1h, 1d, 1w, 1m, 1y'});
+            res.status(400).json({ error: 'Invalid period provided, must be in the format of 1h, 1d, 1w, 1m, 1y' });
             return;
         }
-
     }
 
     try {
-        const queryResult = await wellsMeasurements.wellsMeasurements(wellNum, period, wellsMeasurements.wellsMeasurementsFields.temperature);
+        const queryResult = await wellsMeasurements.wellsMeasurements(wellNum, period, wellsMeasurements.wellsMeasurementsFields[field]);
 
         const formattedResult = queryResult.map(item => ({
             well: item.well,
             timestamp: item._time,
-            temperature: item._value
+            [field]: item._value
         }));
 
         res.status(200).json(formattedResult);
-        console.log(logInfoMsgPrefix(`Well temperature data fetched successfully: ${wellNum}`));
+        console.log(logInfoMsgPrefix(`Well ${field} data fetched successfully: ${wellNum}`));
     } catch (error) {
-        console.log(logErrorMsgPrefix(`Error in fetching well temperature data, ${error}`));
+        console.log(logErrorMsgPrefix(`Error in fetching well ${field} data, ${error}`));
         res.status(500);
     }
 };
+
+/**
+ * @brief This function is used to get the temperature data of a well, the data is fetched from the database
+ * @param wellNum The well number
+ * @query period The period of time to get the data
+ */
+exports.wellsTemperature = (req, res) => getWellData(req, res, 'temperature');
 
 /**
  * @brief This function is used to get the luminosity data of a well, the data is fetched from the database
  * @param wellNum The well number
  * @query period The period of time to get the data
  */
-exports.wellsLuminosity = async (req, res) => {
-    console.log(logInfoMsgPrefix('Payload wells luminosity check'), 'request_body:', req.body);
-    const wellNum = req.params.wellNum;
-    var { period } = req.query; // extract query parameters
-
-    // check if well number is provided, shouldn't happen
-    if(!wellNum){
-        console.log(logWarnMsgPrefix('Well number not provided'));
-        res.status(400).json({error: 'Well number not provided'});
-        return;
-    }
-
-    // check if well number is valid
-    if(wellNum < 1 || wellNum > 16){
-        console.log(logWarnMsgPrefix(`Invalid well number provided: ${wellNum}`));
-        res.status(400).json({error: 'Invalid well number provided, must be between 1 and 16'});
-        return;
-    }
-
-    // check if period is provided, if not, set period to 1h
-    if(!period){
-        console.log(logWarnMsgPrefix('Period not provided, setting to 1h'));
-        period = '1h';
-    }else{
-        // check if period is valid, the period should be in the format of 1h, 1d, 1w, 1m, 1y, the digit can be any number except negative
-        const periodRegex = /^[0-9]\d*[hwdmy]$/;
-        if(!periodRegex.test(period)){
-            console.log(logWarnMsgPrefix(`Invalid period provided: ${period}`));
-            res.status(400).json({error: 'Invalid period provided, must be in the format of 1h, 1d, 1w, 1m, 1y'});
-            return;
-        }
-
-    }
-
-    try {
-        const queryResult = await wellsMeasurements.wellsMeasurements(wellNum, period, wellsMeasurements.wellsMeasurementsFields.luminosity);
-
-        const formattedResult = queryResult.map(item => ({
-            well: item.well,
-            timestamp: item._time,
-            luminosity: item._value
-        }));
-
-        res.status(200).json(formattedResult);
-        console.log(logInfoMsgPrefix(`Well luminosity data fetched successfully: ${wellNum}`));
-    } catch (error) {
-        console.log(logErrorMsgPrefix(`Error in fetching well luminosity data, ${error}`));
-        res.status(500);
-    }
-};
+exports.wellsLuminosity = (req, res) => getWellData(req, res, 'luminosity');
